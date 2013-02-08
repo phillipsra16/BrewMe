a6bf4988bb3bc52a8800c2ed4455fce6ded64c22
Paul Wyatt <wyattpj@SleeveServer0.(none)>
2013-02-07 19:59:41 -0500

    Added virtual environment setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#Setup.py for BrewMe virtual environment
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(
+            name='BrewMe',
+            version='0.0.1',
+            Description='BrewMe website',
+            packages=find_packages(),
+            author='Paul Wyatt',
+            install_requires="""
+                Django==1.4.3
+                ipython==0.13.1
+                MySQL-python
+                django_admin_bootstrapped
+                South
+            """,
+            include_package_data=True,
+        )
