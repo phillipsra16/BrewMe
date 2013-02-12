a840efa8b99d93252a307abc47b3973a9f8d99a6
wyattpj@email.appstate.edu <wyattpj@email.appstate.edu>
2013-02-12 16:49:32 -0500

    Initial User_Auth commit

--- User_Auth/views.py
+++ User_Auth/views.py
@@ -1,1 +1,33 @@
-# Create your views here.
+# User_Authentication views
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
+
+# User login view
+def user_login(request):
+    state = 'Please log in below'
+    # Instantiate these variables to null
+    username = password = ''
+    # If the request is type 'POST'
+    if request.POST:
+        # Set Username and pass
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Use django's built in authentication method
+        user = authenticate(username=username, password=password)
+
+        # Handle the various cases
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+            else:
+                state = "Your account is not active"
+
+        else:
+            state = "Username and/or Password incorrect"
+
+    # Send back a response
+    return render_to_response('auth.html', {
+        'state' : state,
+        'username' : username,
+        })
