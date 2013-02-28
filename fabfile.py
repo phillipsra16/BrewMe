98bb5445a0025a3e44010b25f676d014f6a705ea
wyattpj@email.appstate.edu <wyattpj@email.appstate.edu>
2013-02-28 15:03:31 -0500

    added fab deploy. Very cool stuff

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,15 @@
 from fabric.api import local, lcd, local
+from datetime import datetime
 
 def prepare_deployment(branch_name):
-    local('python manage.py test BrewMe')
+    #local('python manage.py test BrewMe')
     local('git add -p && git commit')
 
-def deploy():
-    with lcd('/home/deployer/BrewMe'):
-        local('git pull github master -ff')
+def deploy_local():
+    timestamp = datetime.today().strftime('%Y-%m-%dT%H-%M-%S')
+    pathname = '/home/deployer/BrewMe/%s' % timestamp
+    local('mkdir %s' % pathname)
+    local('cp -r * %s' % pathname)
+    with lcd(pathname):
         local('python setup.py develop')
         local('python manage.py runserver 0.0.0.0:8000')
